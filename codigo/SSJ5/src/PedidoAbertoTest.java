58b4936f914350a326e2f9b18dfb103f06b257f0
Lucas Cerqueira Azevedo <l31azevedo@gmail.com>
2024-06-19 17:29:22 -0300

    Teste Aberto corrigido

--- codigo/SSJ5/src/PedidoAbertoTest.java
+++ codigo/SSJ5/src/PedidoAbertoTest.java
@@ -1,3 +1,5 @@
+package codigo.SSJ5.src;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -17,12 +19,5 @@ public class PedidoAbertoTest {
     @Test
     public void testCalcularValorFinalSemProduto() {
         assertEquals(0, pedidoAberto.calcularValorFinal(), 0.01);
-    }
-
-    @Test
-    public void testAddProdutoInvalido() {
-        Produto produtoInvalido = new Produto("MACARRÃO", 15);
-        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> pedidoAberto.addProduto(produtoInvalido));
-        assertEquals("Opção não está disponível no menu fechado.", exception.getMessage());
     }
 }
